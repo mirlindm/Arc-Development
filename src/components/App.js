a4c9b590abc04e70f378df246197147fc6d961f2
Mirlind Murati <mirlindmurati777@gmail.com>
2021-04-06 19:05:27 +0300

    Designing the Navbar

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './ui/Header';
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+
+import theme from './ui/Theme';
+
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}> 
+      <Header/>
+      Hello!
+    </ThemeProvider>
+  );
+}
+
+export default App;
