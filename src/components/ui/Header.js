1a827b1b7ea4591c11597a9c2a0d8c101091745d
Mirlind Murati <mirlindmurati777@gmail.com>
2021-04-07 20:53:31 +0300

    Developing the Service Menu

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -7,6 +7,8 @@ import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import Button from '@material-ui/core/Button'
 import {Link} from 'react-router-dom';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import logo from '../../assets/logo.svg'
 
@@ -51,6 +53,18 @@ function ElevationScroll(props) {
           marginLeft: "50px",
           marginRight: "25px",        
           height: "45px",        
+      },
+      menu: {
+          backgroundColor: theme.palette.common.arcBlue,
+          color: "white",
+          borderRadius: "0px",
+      }, 
+      menuItem: {
+          ...theme.typography.tab,
+          opacity: 0.7,
+          "&:hover": {
+              opacity: 1
+          }
       }      
   }))
 
@@ -58,24 +72,46 @@ function ElevationScroll(props) {
 export default function Header(props) {
     const classes = useStyles();
     const [value, setValue] = useState(0);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [open, setOpen] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
 
     const handleChange = (e, value) => {
         setValue(value)
     }
 
-useEffect(() => {
-    if (window.location.pathname === "/" && value !== 0){
-        setValue(0)
-    } else if (window.location.pathname === "/services" && value !== 1){
-        setValue(1)
-    } else if (window.location.pathname === "/revolution" && value !== 2){
-        setValue(2)
-    } else if (window.location.pathname === "/about" && value !== 3){
-        setValue(3)
-    } else if (window.location.pathname === "/contact" && value !== 4){
-        setValue(4)
+    const handleClick = (e) => {
+        setAnchorEl(e.currentTarget);
+        setOpen(true);
     }
-}, [value])
+
+    const handleClose = (e) => {
+        setAnchorEl(null);
+        setOpen(false);
+    }
+
+    const handleMenuItemClick = (e, i) => {
+        setAnchorEl(null);
+        setOpen(false);
+        setSelectedIndex(i);
+    }
+
+    const menuOptions = [{name: "Services", link: "/services"}, {name: "Custom Software Development", link: "/customsoftware"},
+                        {name: "Mobile App Development", link: "/mobileapps"}, {name: "Website Development", link: "/websites"}];
+
+    useEffect(() => {
+        if (window.location.pathname === "/" && value !== 0){
+            setValue(0)
+        } else if (window.location.pathname === "/services" && value !== 1){
+            setValue(1)
+        } else if (window.location.pathname === "/revolution" && value !== 2){
+            setValue(2)
+        } else if (window.location.pathname === "/about" && value !== 3){
+            setValue(3)
+        } else if (window.location.pathname === "/contact" && value !== 4){
+            setValue(4)
+        }
+    }, [value])
 
     return (
         <React.Fragment>
@@ -96,7 +132,10 @@ useEffect(() => {
                             indicatorColor="secondary"
                         >
                             <Tab className={classes.tab} component={Link} to='/' label="Home"/>
-                            <Tab className={classes.tab} component={Link} to='/services' label="Services"/>
+                            <Tab aria-owns={anchorEl ? "simple-menu" : undefined}
+                                aria-haspopup={anchorEl ? "true" : undefined} 
+                                onMouseOver={event => handleClick(event)}
+                                className={classes.tab} component={Link} to='/services' label="Services"/>
                             <Tab className={classes.tab} component={Link} to='/revolution' label="The Revolution"/>
                             <Tab className={classes.tab} component={Link} to='/about' label="About Us"/>
                             <Tab className={classes.tab} component={Link} to='/contact' label="Contact Us"/>
@@ -104,6 +143,28 @@ useEffect(() => {
                         <Button variant="contained" color="secondary" className={classes.button}>
                             Free Estimate
                         </Button>
+                        <Menu id="simple-menu" anchorEl={anchorEl} 
+                            open={open} onClose={handleClose}
+                            MenuListProps={{onMouseLeave: handleClose}}
+                            classes={{paper: classes.menu}}
+                            elevation={0}
+                            >
+                            {menuOptions.map((option, i) => (
+                                <MenuItem key={option} component={Link} to={option.link} 
+                                    classes={{root: classes.menuItem}} 
+                                    onClick={(event) => {handleMenuItemClick(event, i); setValue(1); handleClose()}} 
+                                    selected={i === selectedIndex && value === 1}
+                                    >
+                                {option.name}
+                                </MenuItem>
+                            ))}
+
+                            {/* <MenuItem onClick={() => {handleClose(); setValue(1)}} component={Link} to='/services' classes={{root: classes.menuItem}}>Services</MenuItem>
+                            <MenuItem onClick={() => {handleClose(); setValue(1)}} component={Link} to='/customSoftware' classes={{root: classes.menuItem}}>Custom Software Development</MenuItem>
+                            <MenuItem onClick={() => {handleClose(); setValue(1)}} component={Link} to='/mobileapps' classes={{root: classes.menuItem}}>Mobile App Development</MenuItem>
+                            <MenuItem onClick={() => {handleClose(); setValue(1)}} component={Link} to='/websites' classes={{root: classes.menuItem}}>Website Development</MenuItem> */}
+                        </Menu>
+
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
