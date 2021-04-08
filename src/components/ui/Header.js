6b3a32871023db2171b7a1ccb405b2d375436723
Mirlind Murati <mirlindmurati777@gmail.com>
2021-04-08 22:30:39 +0300

    Making the Header responsive using Media Queries

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -9,6 +9,8 @@ import Button from '@material-ui/core/Button'
 import {Link} from 'react-router-dom';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import {useTheme} from '@material-ui/core/styles';
 
 import logo from '../../assets/logo.svg'
 
@@ -28,10 +30,22 @@ function ElevationScroll(props) {
   const useStyles = makeStyles(theme => ({
       toolbarMargin: {
           ...theme.mixins.toolbar,
-          marginBottom: '3em'
+          marginBottom: "3em",
+          [theme.breakpoints.down("md")] : {
+            marginBottom: "2em",
+          },
+          [theme.breakpoints.down("xs")] : {
+            marginBottom: "1.25em"  
+          }
       },
       logo: {
-          height: "8em"
+          height: "8em",
+          [theme.breakpoints.down("md")] : {
+            height: "7em",
+          },
+          [theme.breakpoints.down("xs")] : {
+            height: "5.5em"  
+          }
       },
       logoContainer: {
         padding: 0,
@@ -71,6 +85,8 @@ function ElevationScroll(props) {
 
 export default function Header(props) {
     const classes = useStyles();
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.down("md"));
     const [value, setValue] = useState(0);
     const [anchorEl, setAnchorEl] = useState(null);
     const [open, setOpen] = useState(false);
@@ -99,19 +115,7 @@ export default function Header(props) {
     const menuOptions = [{name: "Services", link: "/services"}, {name: "Custom Software Development", link: "/customsoftware"},
                         {name: "Mobile App Development", link: "/mobileapps"}, {name: "Website Development", link: "/websites"}];
 
-    useEffect(() => {
-        // if (window.location.pathname === "/" && value !== 0){
-        //     setValue(0)
-        // } else if (window.location.pathname === "/services" && value !== 1){
-        //     setValue(1)
-        // } else if (window.location.pathname === "/revolution" && value !== 2){
-        //     setValue(2)
-        // } else if (window.location.pathname === "/about" && value !== 3){
-        //     setValue(3)
-        // } else if (window.location.pathname === "/contact" && value !== 4){
-        //     setValue(4)
-        // }
-
+    useEffect(() => {        
         switch(window.location.pathname) {
             case "/": 
                 if(value !== 0){
@@ -167,6 +171,49 @@ export default function Header(props) {
         }
     }, [value])
 
+    const tabs = (
+        <React.Fragment>
+            <Tabs 
+                className={classes.tabContainer} 
+                value={value} 
+                onChange={handleChange}
+                indicatorColor="secondary"
+            >
+                <Tab className={classes.tab} component={Link} to='/' label="Home"/>
+                <Tab aria-owns={anchorEl ? "simple-menu" : undefined}
+                    aria-haspopup={anchorEl ? "true" : undefined} 
+                    onMouseOver={event => handleClick(event)}
+                    className={classes.tab} component={Link} to='/services' label="Services"
+                />
+                <Tab className={classes.tab} component={Link} to='/revolution' label="The Revolution"/>
+                <Tab className={classes.tab} component={Link} to='/about' label="About Us"/>
+                <Tab className={classes.tab} component={Link} to='/contact' label="Contact Us"/>
+            </Tabs>
+
+            <Button variant="contained" color="secondary" className={classes.button}>
+                Free Estimate
+            </Button>
+
+            <Menu id="simple-menu" anchorEl={anchorEl} 
+                open={open} onClose={handleClose}
+                MenuListProps={{onMouseLeave: handleClose}}
+                classes={{paper: classes.menu}}
+                elevation={0}
+            >
+
+            {menuOptions.map((option, i) => (
+                <MenuItem key={option} component={Link} to={option.link} 
+                    classes={{root: classes.menuItem}} 
+                    onClick={(event) => {handleMenuItemClick(event, i); setValue(1); handleClose()}} 
+                    selected={i === selectedIndex && value === 1}
+                >
+                    {option.name}
+                </MenuItem>
+            ))}                            
+            </Menu>                                    
+        </React.Fragment>
+    )
+
     return (
         <React.Fragment>
             <ElevationScroll> 
@@ -179,46 +226,9 @@ export default function Header(props) {
                         >
                             <img alt="Company Logo" className={classes.logo} src={logo} />
                         </Button>                    
-                        <Tabs 
-                            className={classes.tabContainer} 
-                            value={value} 
-                            onChange={handleChange}
-                            indicatorColor="secondary"
-                        >
-                            <Tab className={classes.tab} component={Link} to='/' label="Home"/>
-                            <Tab aria-owns={anchorEl ? "simple-menu" : undefined}
-                                aria-haspopup={anchorEl ? "true" : undefined} 
-                                onMouseOver={event => handleClick(event)}
-                                className={classes.tab} component={Link} to='/services' label="Services"/>
-                            <Tab className={classes.tab} component={Link} to='/revolution' label="The Revolution"/>
-                            <Tab className={classes.tab} component={Link} to='/about' label="About Us"/>
-                            <Tab className={classes.tab} component={Link} to='/contact' label="Contact Us"/>
-                        </Tabs>
-                        <Button variant="contained" color="secondary" className={classes.button}>
-                            Free Estimate
-                        </Button>
-                        <Menu id="simple-menu" anchorEl={anchorEl} 
-                            open={open} onClose={handleClose}
-                            MenuListProps={{onMouseLeave: handleClose}}
-                            classes={{paper: classes.menu}}
-                            elevation={0}
-                            >
-                            {menuOptions.map((option, i) => (
-                                <MenuItem key={option} component={Link} to={option.link} 
-                                    classes={{root: classes.menuItem}} 
-                                    onClick={(event) => {handleMenuItemClick(event, i); setValue(1); handleClose()}} 
-                                    selected={i === selectedIndex && value === 1}
-                                    >
-                                {option.name}
-                                </MenuItem>
-                            ))}
-
-                            {/* <MenuItem onClick={() => {handleClose(); setValue(1)}} component={Link} to='/services' classes={{root: classes.menuItem}}>Services</MenuItem>
-                            <MenuItem onClick={() => {handleClose(); setValue(1)}} component={Link} to='/customSoftware' classes={{root: classes.menuItem}}>Custom Software Development</MenuItem>
-                            <MenuItem onClick={() => {handleClose(); setValue(1)}} component={Link} to='/mobileapps' classes={{root: classes.menuItem}}>Mobile App Development</MenuItem>
-                            <MenuItem onClick={() => {handleClose(); setValue(1)}} component={Link} to='/websites' classes={{root: classes.menuItem}}>Website Development</MenuItem> */}
-                        </Menu>
-
+                        {
+                            matches ? null : tabs
+                        }   
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
