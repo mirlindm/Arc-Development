ff1b288b0817357b580d89f3b205ca3824b105cb
Mirlind Murati <mirlindmurati777@gmail.com>
2021-04-14 22:34:52 +0300

    Setting up the Service Block

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,8 +5,10 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import ButtonArrow from './ui/ButtonArrow';
 import Typography from '@material-ui/core/Typography';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import animationData from '../animations/landinganimation/data';
+import customSoftwareIcon from '../assets/Custom Software Icon.svg';
 
 const useStyles = makeStyles(theme => ({
     animation: {
@@ -34,16 +36,19 @@ const useStyles = makeStyles(theme => ({
         marginTop: "1em",
     },
     learnButtonHero: {
-        borderColor: theme.palette.common.arcBlue,
-        color: theme.palette.common.arcBlue,
-        borderWidth: 2,
-        textTransform: "none",
-        borderRadius: 50,
-        fontFamily: "Roboto",
-        fontWeight: "bold",
+        ...theme.typography.learnButton,        
         fontSize: "0.9rem",
         height: 45,
         width: 145,
+    },
+    learnButton : {
+        ...theme.typography.learnButton,
+        fontSize: "0.7rem",
+        height: 35,
+        padding: 5,
+        [theme.breakpoints.down("sm")]: {
+            marginBottom: "2em"
+        }
     },
     mainContainer: {
         marginTop: "5em",
@@ -60,12 +65,32 @@ const useStyles = makeStyles(theme => ({
         [theme.breakpoints.down("xs")]: {
             marginLeft: 0,
         }
+    },
+    specialText: {
+        fontFamily: "Pacifico",
+        color: theme.palette.common.arcOrange,
+    },
+    subtitle: {
+        marginBottom: "1em",
+    },
+    icon: {
+        marginLeft: "2em",
+        [theme.breakpoints.down("xs")]: {
+            marginLeft: 0,
+        }
+    },
+    serviceContainer: {
+        marginTop: "12em",
+        [theme.breakpoints.down("sm")]: {
+            padding: 25
+        }
     }
 }))
 
 export default function LandingPage() {
     const classes = useStyles();
     const theme = useTheme();
+    const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
 
     const defaultOptions = {
         loop: true,
@@ -78,7 +103,7 @@ export default function LandingPage() {
 
     return (
         <Grid container direction="column" className={classes.mainContainer}>
-            <Grid item>
+            <Grid item> {/*-----Hero Block-----*/}
                 <Grid container direction="row" justify="flex-end" alignItems="center">
                     <Grid item sm className={classes.heroTextContainer}>
                        <Typography variant="h2" align="center"> Bringing West Coast Technology <br/> to the Midwest</Typography>
@@ -100,6 +125,33 @@ export default function LandingPage() {
                             height={"100%"}
                             width={"100%"}                            
                         /> 
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item> {" "}
+            {/*-----Services Block-----*/}
+                <Grid container direction="row" 
+                    className={classes.serviceContainer}
+                    justify={matchesSM ? "center" : undefined}
+                >
+                    <Grid item style={{marginLeft: matchesSM ? 0 : "5em", textAlign: matchesSM ? "center" : undefined}}>
+                        <Typography variant="h4">
+                            Custom Software Development
+                        </Typography>
+                        <Typography variant="subtitle1" className={classes.subtitle}>
+                            Save Energy. Save Time. Save Money.
+                        </Typography>
+                        <Typography variant="subtitle1">
+                            Complete digital solutions, from investigation to{" "} 
+                            <span className={classes.specialText}>celebration.</span>
+                        </Typography>
+                        <Button className={classes.learnButton} variant="outlined">
+                            <span style={{marginRight: 10}}> Learn More </span> 
+                            <ButtonArrow width={10} height={10} fill={theme.palette.common.arcBlue} />
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        <img className={classes.icon} alt="Custom Software Icon" src={customSoftwareIcon} />
                     </Grid>
                 </Grid>
             </Grid>
